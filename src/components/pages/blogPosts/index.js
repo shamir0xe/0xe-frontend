9f5792146a7b3800e862aad3f8c209fe698b0f6a
shamir0xe <shamir0xe@gmail.com>
2022-11-23 11:04:16 +0330

    feat: first page developement started

---
+++ src/components/pages/blogPosts/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import classes from './index.module.css'
+import contents from './contents'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
+
+function BlogPosts(props) {
+    return (
+        <header>
+            <div className={classes.Container}>
+                <img
+                    src={contents.imgs.logo.src}
+                    className={classes.XelluLogo}
+                    alt={contents.imgs.logo.alt}
+                />
+                <nav className={classes.Navigator}>
+                    {[...Array(contents.txts.items.length).keys()].map(
+                        (value, index) => {
+                            return (
+                                <button
+                                    key={`btn-${index}`}
+                                    className={classes.Items}
+                                >
+                                    {contents.txts.items[value]}
+                                </button>
+                            )
+                        }
+                    )}
+                    <div className={classes.SearchWrapper}>
+                        <input />
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </div>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default BlogPosts
---
+++ src/components/pages/topbar/contents.js
@@ -0,0 +1,20 @@
+const contents = {
+    imgs: {
+        logo: {
+            src: require("src/../assets/xellu-logo.png"), 
+            alt: "xellu logo",
+        }
+    },
+    txts: {
+        items: [
+            'HOME',
+            'STORY',
+            'ROADMAP',
+            'ABOUT',
+            'MINT',
+        ]
+    }
+   
+};
+
+export default contents;
